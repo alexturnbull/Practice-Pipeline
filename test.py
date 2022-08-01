faffa9924a81fecc37db3b5c8b9f4f01918fbcb5
alexturnbull <alexturnbull1@gmail.com>
2022-08-01 10:53:09 +0100

    Sever connection established

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
-import pypyodbc as odbc
+import pyodbc as odbc
 import os 
 
-DRIVER_NAME = '{SQL Server Native Client 11.0}'
+DRIVER_NAME = '{SQL Server}'
 SERVER_NAME = 'PROMETHEUS\SQLEXPRESS'
 DATABASE_NAME ='AdventureWorksDW2019'
-#pwd = os.environ.get('PASSOS')
-#uid = os.environ.get('USEROS')
+pwd = os.environ.get('PASSOS')
+uid = os.environ.get('USEROS')
 
 
-connection_string = f"""DRIVER={DRIVER_NAME}; SERVER= {SERVER_NAME}; DATABASE={DATABASE_NAME}; Trust_Connection=yes'"""
-
-
+connection_string = f"""DRIVER={DRIVER_NAME}; SERVER={SERVER_NAME}; DATABASE={DATABASE_NAME}; Trust_connection='yes'"""
 conn = odbc.connect(connection_string)
 print(conn)
