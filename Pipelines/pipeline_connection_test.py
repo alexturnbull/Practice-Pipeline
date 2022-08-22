da6580494a568daa1379b21d9ec7b3e09c8ca60d
alexturnbull <alexturnbull1@gmail.com>
2022-08-22 12:15:52 +0100

    succesfull read

---
+++ Pipelines/pipeline_connection_test.py
@@ -0,0 +1,19 @@
+import pypyodbc as odbc
+import os 
+import pandas as pd
+import pandas.io.sql
+from sqlalchemy.engine import URL
+from sqlalchemy import create_engine
+
+DRIVER_NAME = '{SQL Server}'
+SERVER_NAME = 'PROMETHEUS\SQLEXPRESS'
+DATABASE_NAME ='AdventureWorksDW2019'
+PASS = os.environ.get('PASSOS')
+USER = os.environ.get('USEROS')
+
+connection_string = f"""DRIVER={DRIVER_NAME}; SERVER={SERVER_NAME}; DATABASE={DATABASE_NAME}; Trust_Connection=yes"""
+connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+engine = create_engine(connection_url)
+
+df = pd.read_sql_query('SELECT TOP 10 * FROM dbo.DimAccount', engine)
+print(df)
