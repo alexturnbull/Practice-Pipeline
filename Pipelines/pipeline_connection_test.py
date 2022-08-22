5bb7985f4a3ad3da0ab8839a27f67b1d81f52f20
alexturnbull <alexturnbull1@gmail.com>
2022-08-22 12:23:08 +0100

    read using etl user

--- Pipelines/pipeline_connection_test.py
+++ Pipelines/pipeline_connection_test.py
@@ -1,7 +1,6 @@
 import pypyodbc as odbc
 import os 
 import pandas as pd
-import pandas.io.sql
 from sqlalchemy.engine import URL
 from sqlalchemy import create_engine
 
@@ -11,8 +10,10 @@ DATABASE_NAME ='AdventureWorksDW2019'
 PASS = os.environ.get('PASSOS')
 USER = os.environ.get('USEROS')
 
-connection_string = f"""DRIVER={DRIVER_NAME}; SERVER={SERVER_NAME}; DATABASE={DATABASE_NAME}; Trust_Connection=yes"""
-connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+#connection_string = f"""DRIVER={DRIVER_NAME}; SERVER={SERVER_NAME}; DATABASE={DATABASE_NAME}; Trust_Connection=yes"""
+connection_string_etl = f"""DRIVER={DRIVER_NAME}; SERVER={SERVER_NAME}; DATABASE={DATABASE_NAME}; UID={USER}; PWD={PASS}"""
+
+connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string_etl})
 engine = create_engine(connection_url)
 
 df = pd.read_sql_query('SELECT TOP 10 * FROM dbo.DimAccount', engine)
