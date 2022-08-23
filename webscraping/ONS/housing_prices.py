55a8026289ace2a604171e2c6052150700ed626d
alexturnbull <alexturnbull1@gmail.com>
2022-08-23 13:58:22 +0100

    reading housing prices from ONS

---
+++ webscraping/ONS/housing_prices.py
@@ -0,0 +1,11 @@
+import requests 
+
+downloadUrl = 'https://www.ons.gov.uk/generator?uri=/economy/inflationandpriceindices/bulletins/housepriceindex/february2022/61b62a24&format=csv'
+
+
+with requests.get(downloadUrl) as rq:
+    with open('housing_prices.csv', 'wb') as file:
+        file.write(rq.content)
+
+  
+
