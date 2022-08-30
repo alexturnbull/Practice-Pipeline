2331d505c65f4017eb89889e872176fd120da7f2
alexturnbull <alexturnbull1@gmail.com>
2022-08-30 10:42:22 +0100

    starting housing pipeline

--- webscraping/ONS/housing_prices.py
+++ webscraping/ONS/housing_prices.py
@@ -1,9 +1,11 @@
 import requests 
-
+from datetime import date 
 downloadUrl = 'https://www.ons.gov.uk/generator?uri=/economy/inflationandpriceindices/bulletins/housepriceindex/february2022/61b62a24&format=csv'
 
+today = date.today()
+print(today)
 
 with requests.get(downloadUrl) as rq:
-    with open("C:\\Data\\ONS\\housing_prices\\housing_prices.csv", 'wb') as file:
-        file.write(rq.content)
+      with open(f"C:\\Data\\ONS\\housing_prices\\housing_prices_{today}.csv", 'wb') as file:
+          file.write(rq.content)
     
